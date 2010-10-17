da7b3920abf4ce3fb44533109c774c11cbb42ee8
hehua <1518541626@qq.com>
2010-10-18 00:35:04 +0800

    initialize SSB project

---
+++ app/controllers/application.rb
@@ -0,0 +1,10 @@
+# Filters added to this controller apply to all controllers in the application.
+# Likewise, all the methods added will be available for all controllers.
+
+class ApplicationController < ActionController::Base
+  helper :all # include all helpers, all the time
+
+  # See ActionController::RequestForgeryProtection for details
+  # Uncomment the :secret if you're not using the cookie session store
+  protect_from_forgery # :secret => '717db2e00dd27e3a7b842f808e8c5e65'
+end
